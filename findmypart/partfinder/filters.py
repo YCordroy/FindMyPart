4f9291c38085ac218062d42ff05eceaeb46d1bed
Виталий Шалыгин <vtz.2@yandex.ru>
2024-07-14 10:43:26 +0700

    Доработана функция фильтра поиска

--- findmypart/partfinder/filters.py
+++ findmypart/partfinder/filters.py
@@ -1,32 +1,37 @@
-import json
-
 from django.db.models import Q
 
 
 def search_parts_filter(data):
     """Формирование подзапроса для фильтра запчастей"""
     query = Q()
+
     # Добавление фильтров из запроса
-    if data.get('model_name'):
-        query &= Q(model__name__icontains=data['model_name'])
-    if data.get('mark_name'):
-        query &= Q(mark__name__icontains=data['mark_name'])
-    if data.get('part_name'):
-        query &= Q(name__icontains=data['part_name'])
-    if data.get('price_gte'):
-        query &= Q(price__gte=data['price_gte'])
-    if data.get('price_lte'):
-        query &= Q(price__lte=data['price_lte'])
+    filter_map = {
+        'model_name': 'model__name__icontains',
+        'mark_name': 'mark__name__icontains',
+        'part_name': 'name__icontains',
+        'price_gte': 'price__gte',
+        'price_lte': 'price__lte'
+    }
+    params_filter = {
+        filter_map[key]: value
+        for key, value in data.items()
+        if key in filter_map
+    }
+    query &= Q(**params_filter)
+
+    # Обработка вложенных параметров
     if data.get('params'):
-        # Обработка вложенных параметров
         request_params = data['params']
         params_encode = {
             f'json_data__{param}__icontains': value
             for param, value in request_params.items()
         }
         query &= Q(**params_encode)
+
+    # Обработка нескольких марок
     if data.get('mark_list'):
-        mark_list = json.loads(data['mark_list'])
+        mark_list = data['mark_list']
         query &= Q(mark__id__in=mark_list)
 
     return query
