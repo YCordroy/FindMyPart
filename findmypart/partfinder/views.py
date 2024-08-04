8d0f08be913f7bb07c677266cdb0680aec05d48c
Виталий Шалыгин <vtz.2@yandex.ru>
2024-08-04 18:59:05 +0700

    Добавлен README, поправлены импорты

--- findmypart/partfinder/views.py
+++ findmypart/partfinder/views.py
@@ -1,14 +1,14 @@
 import json
 
 from django.http import JsonResponse
-
-from .filters import search_parts_filter
-from .models import Mark, Model, Part
-from .serializers import serialize_search_part
 from django.core.paginator import Paginator
 from django.conf import settings
 from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_exempt
+
+from .filters import search_parts_filter
+from .models import Mark, Model, Part
+from .serializers import serialize_search_part
 
 
 @require_GET
